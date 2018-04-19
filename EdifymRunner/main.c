c938615cb7dfe0cbe30364a174eba13d3ea1f6db
Michael de Lang <kingoipo@gmail.com>
2018-04-19 16:21:55 +0200

    Add debug prints

--- EdifymRunner/main.c
+++ EdifymRunner/main.c
@@ -1,11 +1,13 @@
-#include "m5ops.h"
+#include "gem5/m5ops.h"
 #include "executable_task.h"
 #include <string.h>
+#include <stdio.h>
 
 extern task tasks_to_execute;
 
 task* find_task_by_name(task* t, char const *name) {
     while(t != NULL) {
+        printf("checking for %s in task %s\n", name, t->name);
         if (strcmp(t->name, name) == 0) {
             return t;
         }
@@ -18,12 +20,13 @@ task* find_task_by_name(task* t, char const *name) {
 int main( void ) {
     functionPtr function;
     char *ordered_task_names[TASK_SIZE] = TASKS;
+    printf("Started with TASK_SIZE %i\n", TASK_SIZE);
 
     for (int i = 0; i < TASK_SIZE; i++) {
         task* t = find_task_by_name(&tasks_to_execute, ordered_task_names[i]);
 
         if(t == NULL) {
-            printf("Horrible disaster");
+            printf("Horrible disaster\n");
             return -1;
         }
 
