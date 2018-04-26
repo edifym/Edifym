9f720c2963a79fe1c7473049362fcf5d5976608c
Michael de Lang <kingoipo@gmail.com>
2018-04-26 10:26:51 +0200

    Init compile task

--- MainConfig.py
+++ MainConfig.py
@@ -1,5 +1,7 @@
 class MainConfig:
     gem5_path: str = ""
+    library_name: str = ""
 
     def __init__(self, json_data):
         self.gem5_path = json_data['gem5_path']
+        self.library_name = json_data['library_name']
--- Tasks/CompileSingleSimulationTask.py
+++ Tasks/CompileSingleSimulationTask.py
@@ -1,6 +1,23 @@
+import MainConfig
+from BenchmarkConfig import Benchmark
 from Tasks.ITask import ITask
+import subprocess
+import os
+from slinkie import Slinkie
 
 
 class CompileSingleSimulationITask(ITask):
+    main_config: MainConfig
+    benchmark: Benchmark
+
+    def __init__(self, main_config: MainConfig, benchmark: Benchmark):
+        self.main_config = main_config
+        self.benchmark = benchmark
+
     def execute(self):
-        pass
+        new_env = os.environ.copy()
+        new_env['M5_DIR'] = self.main_config.gem5_path
+        new_env['LIBRARY_UNDER_TEST'] = self.main_config.library_name
+        new_env['TASK_SIZE'] = len(self.benchmark.tasks)
+        new_env['TASKS'] = '{' + ', '.join(Slinkie(self.benchmark.tasks).map(lambda it: f"\"{it.name}\"")) + '}'
+        print f"{new_env['TASKS']}"
