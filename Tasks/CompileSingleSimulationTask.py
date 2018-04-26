115bf7abcc12b058486f7b50f1334bf9acab77d3
Michael de Lang <kingoipo@gmail.com>
2018-04-26 14:05:50 +0200

    Make compilation from python work

--- Tasks/CompileSingleSimulationTask.py
+++ Tasks/CompileSingleSimulationTask.py
@@ -5,6 +5,7 @@ import subprocess
 import os
 import sys
 from slinkie import Slinkie
+from typing import List, Dict
 
 
 class CompileSingleSimulationTask(ITask):
@@ -15,20 +16,26 @@ class CompileSingleSimulationTask(ITask):
         self.main_config = main_config
         self.benchmark = benchmark
 
-    def execute(self):
-        new_env = os.environ.copy()
-        new_env['M5_DIR'] = self.main_config.gem5_path
-        new_env['LIBRARY_UNDER_TEST'] = self.main_config.library_name
-        new_env['TASK_SIZE'] = len(self.benchmark.tasks)
-        new_env['TASKS'] = '{' + Slinkie(self.benchmark.tasks).map(lambda it: f"\"{it.name}\"").join(', ') + '}'
-        try:
-            proc = subprocess.Popen('cmake .', env=new_env)
+    def run_command(self, command: List[str], new_env: Dict[str, str]):
+            proc = subprocess.Popen(command, cwd='./EdifymRunner', env=new_env, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             output, error = proc.communicate()
             if output:
                 print(f"cmake output> {proc.returncode} {output}")
             if error:
                 print(f"cmake error> {proc.returncode} {error.strip()}")
+
+    def execute(self):
+        new_env = os.environ.copy()
+        new_env['M5_DIR'] = self.main_config.gem5_path
+        new_env['LIBRARY_UNDER_TEST'] = self.main_config.library_name
+        new_env['TASK_SIZE'] = str(len(self.benchmark.tasks))
+        new_env['TASKS'] = '{' + Slinkie(self.benchmark.tasks).map(lambda it: f"\"{it.name}\"").join(', ') + '}'
+        try:
+            self.run_command(['cmake', '.'], new_env)
+            self.run_command(['make'], new_env)
         except OSError as e:
             print(f"OSError> {e.errno} {e.strerror} {e.filename}")
+        except TypeError as e:
+            print(f"TypeError> {e}")
         except:
             print(f"Error> {sys.exc_info()[0]}")
