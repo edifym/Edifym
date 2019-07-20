0210c980cfbfb41c2dcb82a259fa4da545bab4a9
Michael de Lang <kingoipo@gmail.com>
2019-07-20 20:17:49 +0200

    Add sim_insts to static/dynamic comparison

--- collect_stats_single.py
+++ collect_stats_single.py
@@ -4,32 +4,44 @@ from CommandHelper import CommandHelper
 
 
 if __name__ == "__main__":
-    stats_static = CommandHelper.run_command_output(['awk', '/sim_sec/ {print $2}', f'stats_static.txt'], './').splitlines()
-    stats_dynamic = CommandHelper.run_command_output(['awk', '/sim_sec/ {print $2}', f'stats_dynamic.txt'], './').splitlines()
+    sim_secs_static = CommandHelper.run_command_output(['awk', '/sim_sec/ {print $2}', f'stats_static.txt'], './').splitlines()
+    sim_secs_dynamic = CommandHelper.run_command_output(['awk', '/sim_sec/ {print $2}', f'stats_dynamic.txt'], './').splitlines()
 
-    if len(stats_static) != 27:
-        print(f'static stats incorrect stats length {len(stats_static)} {stats_static}')
+    sim_insts_static = CommandHelper.run_command_output(['awk', '/sim_insts/ {print $2}', f'stats_static.txt'], './').splitlines()
+    sim_insts_dynamic = CommandHelper.run_command_output(['awk', '/sim_insts/ {print $2}', f'stats_dynamic.txt'], './').splitlines()
+
+    if len(sim_secs_static) != 27:
+        print(f'static stats incorrect stats length {len(sim_secs_static)} {sim_secs_static}')
         sys.exit(-1)
 
-    if len(stats_dynamic) != 27:
-        print(f'static stats incorrect stats length {len(stats_dynamic)} {stats_dynamic}')
+    if len(sim_secs_dynamic) != 27:
+        print(f'static stats incorrect stats length {len(sim_secs_dynamic)} {sim_secs_dynamic}')
         sys.exit(-1)
 
-    totals_static = 0
-    totals_dynamic = 0
+    total_sim_sec_static = 0
+    total_sim_sec_dynamic = 0
+    total_sim_insts_static = 0
+    total_sim_insts_dynamic = 0
     prev_time = 0
+    prev_insts = 0
 
     for i in range(26):
         if i % 2 == 0:
-            prev_time = int(stats_static[i][2:])
+            prev_time = int(sim_secs_static[i][2:])
+            prev_insts = int(sim_insts_static[i])
         else:
-            totals_static += int(stats_static[i][2:]) - int(stats_static[i - 1][2:])
+            #print(f'adding {int(stats_static[i][2:]):,} {int(stats_static[i-1][2:]):,} {int(stats_static[i][2:]) - int(stats_static[i - 1][2:]):,}')
+            total_sim_sec_static += int(sim_secs_static[i][2:]) - int(sim_secs_static[i - 1][2:])
+            total_sim_insts_static += int(sim_insts_static[i]) - int(sim_insts_static[i - 1])
 
     for i in range(26):
         if i % 2 == 0:
-            prev_time = int(stats_dynamic[i][2:])
+            prev_time = int(sim_secs_dynamic[i][2:])
+            prev_insts = int(sim_insts_dynamic[i])
         else:
-            totals_dynamic += int(stats_dynamic[i][2:]) - int(stats_dynamic[i - 1][2:])
+            #print(f'adding {int(stats_dynamic[i][2:]):,} {int(stats_dynamic[i - 1][2:]):,} {int(stats_dynamic[i][2:]) - int(stats_dynamic[i - 1][2:]):,}')
+            total_sim_sec_dynamic += int(sim_secs_dynamic[i][2:]) - int(sim_secs_dynamic[i - 1][2:])
+            total_sim_insts_dynamic += int(sim_insts_dynamic[i]) - int(sim_insts_dynamic[i - 1])
 
-    print(f'static: {totals_static}')
-    print(f'dynamic: {totals_dynamic}')
+    print(f'static: {total_sim_sec_static} - {total_sim_insts_static}')
+    print(f'dynamic: {total_sim_sec_dynamic} - {total_sim_insts_dynamic}')
