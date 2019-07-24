5fc9ec3626f3d70075a21f7528c9293d69518624
Michael de Lang <kingoipo@gmail.com>
2019-07-24 20:37:29 +0200

    Distinguish static runner between pointer indirection and direct function call

--- EdifymRunner/main_static_without_function_pointer.c
+++ EdifymRunner/main_static_without_function_pointer.c
@@ -1,5 +1,4 @@
 #include "gem5/m5ops.h"
-#include "executable_task.h"
 #include <stdio.h>
 
 void program_init(void);
@@ -42,49 +41,34 @@ void servo_transmit(void);
 void test_ppm_task(void);
 int init_test_ppm_task(int arg_count, int *task_args);
 
-extern task _altitude_control_task;
-extern task _climb_control_task;
-extern task _link_fbw_send;
-extern task _navigation_task;
-extern task _radio_control_task;
-extern task _receive_gps_data_task;
-extern task _reporting_task;
-extern task _stabilisation_task;
-extern task _check_failsafe_task;
-extern task _check_mega128_values_task;
-extern task _send_data_to_autopilot_task;
-extern task _servo_transmit;
-extern task _test_ppm_task;
-
 
 #define EXECUTE_TASK_WITH_INIT(task, valcount, valone, valtwo, valthree, valfour, valfive, valsix) \
     values[0] = valone; values[1] = valtwo; values[2] = valthree; values[3] = valfour; values[4] = valfive; values[5] = valsix; \
     /*printf("running " #task " with %i count %i %i %i %i %i %i\n", valcount, valone, valtwo, valthree, valfour, valfive, valsix);*/ \
-    task.init(valcount, values); \
-    m5_dump_stats(0, 0); task.function(); m5_dump_stats(0, 0); \
-
+    init_ ## task(valcount, values); \
+    m5_dump_stats(0, 0); task(); m5_dump_stats(0, 0); \
 
 #define EXECUTE_TASK(task) \
-    m5_dump_stats(0, 0); task.function(); m5_dump_stats(0, 0);
+    m5_dump_stats(0, 0); task(); m5_dump_stats(0, 0);
 
 int main( int argc, char *argv[] ) {
     int values[6];
 
     program_init();
 
-    EXECUTE_TASK_WITH_INIT(_test_ppm_task, 4, 1, 0, 1, 1, 0, 0)
-    EXECUTE_TASK(_servo_transmit)
-    EXECUTE_TASK_WITH_INIT(_send_data_to_autopilot_task, 4, 0, 1, 1, 1, 0, 0)
-    EXECUTE_TASK_WITH_INIT(_check_mega128_values_task, 4, 0, 1, 0, 1, 0, 0)
-    EXECUTE_TASK_WITH_INIT(_check_failsafe_task, 1, 0, 0, 0, 0, 0, 0)
-    EXECUTE_TASK_WITH_INIT(_stabilisation_task, 1, 1, 0, 0, 0, 0, 0)
-    EXECUTE_TASK(_reporting_task)
-    EXECUTE_TASK_WITH_INIT(_receive_gps_data_task, 6, 0, 3, 0, 0, 0, 6)
-    EXECUTE_TASK_WITH_INIT(_radio_control_task, 5, 1, 4, 1, 1, 0, 0)
-    EXECUTE_TASK_WITH_INIT(_navigation_task, 2, 0, 3, 0, 0, 0, 0)
-    EXECUTE_TASK_WITH_INIT(_link_fbw_send, 1, 1, 0, 0, 0, 0, 0)
-    EXECUTE_TASK_WITH_INIT(_climb_control_task, 5, 0, 1, 7, 6, 1, 0)
-    EXECUTE_TASK_WITH_INIT(_altitude_control_task, 1, 1, 0, 0, 0, 0, 0)
+    EXECUTE_TASK_WITH_INIT(test_ppm_task, 4, 1, 0, 1, 1, 0, 0)
+    EXECUTE_TASK(servo_transmit)
+    EXECUTE_TASK_WITH_INIT(send_data_to_autopilot_task, 4, 0, 1, 1, 1, 0, 0)
+    EXECUTE_TASK_WITH_INIT(check_mega128_values_task, 4, 0, 1, 0, 1, 0, 0)
+    EXECUTE_TASK_WITH_INIT(check_failsafe_task, 1, 0, 0, 0, 0, 0, 0)
+    EXECUTE_TASK_WITH_INIT(stabilisation_task, 1, 1, 0, 0, 0, 0, 0)
+    EXECUTE_TASK(reporting_task)
+    EXECUTE_TASK_WITH_INIT(receive_gps_data_task, 6, 0, 3, 0, 0, 0, 6)
+    EXECUTE_TASK_WITH_INIT(radio_control_task, 5, 1, 4, 1, 1, 0, 0)
+    EXECUTE_TASK_WITH_INIT(navigation_task, 2, 0, 3, 0, 0, 0, 0)
+    EXECUTE_TASK_WITH_INIT(link_fbw_send, 1, 1, 0, 0, 0, 0, 0)
+    EXECUTE_TASK_WITH_INIT(climb_control_task, 5, 0, 1, 7, 6, 1, 0)
+    EXECUTE_TASK_WITH_INIT(altitude_control_task, 1, 1, 0, 0, 0, 0, 0)
 
     return 0;
 }
--- collect_stats_single.py
+++ collect_stats_single.py
@@ -5,13 +5,19 @@ from CommandHelper import CommandHelper
 
 if __name__ == "__main__":
     sim_secs_static = CommandHelper.run_command_output(['awk', '/sim_sec/ {print $2}', f'stats_static.txt'], './').splitlines()
+    sim_secs_static_nofp = CommandHelper.run_command_output(['awk', '/sim_sec/ {print $2}', f'stats_static_nofp.txt'], './').splitlines()
     sim_secs_dynamic = CommandHelper.run_command_output(['awk', '/sim_sec/ {print $2}', f'stats_dynamic.txt'], './').splitlines()
 
     sim_insts_static = CommandHelper.run_command_output(['awk', '/sim_insts/ {print $2}', f'stats_static.txt'], './').splitlines()
+    sim_insts_static_nofp = CommandHelper.run_command_output(['awk', '/sim_insts/ {print $2}', f'stats_static_nofp.txt'], './').splitlines()
     sim_insts_dynamic = CommandHelper.run_command_output(['awk', '/sim_insts/ {print $2}', f'stats_dynamic.txt'], './').splitlines()
 
     if len(sim_secs_static) != 27:
         print(f'static stats incorrect stats length {len(sim_secs_static)} {sim_secs_static}')
+        sys.exit(-1)
+
+    if len(sim_secs_static_nofp) != 27:
+        print(f'static_nofp stats incorrect stats length {len(sim_secs_static_nofp)} {sim_secs_static_nofp}')
         sys.exit(-1)
 
     if len(sim_secs_dynamic) != 27:
@@ -19,8 +25,10 @@ if __name__ == "__main__":
         sys.exit(-1)
 
     total_sim_sec_static = 0
+    total_sim_sec_static_nofp = 0
     total_sim_sec_dynamic = 0
     total_sim_insts_static = 0
+    total_sim_insts_static_nofp = 0
     total_sim_insts_dynamic = 0
     prev_time = 0
     prev_insts = 0
@@ -30,18 +38,34 @@ if __name__ == "__main__":
             prev_time = int(sim_secs_static[i][2:])
             prev_insts = int(sim_insts_static[i])
         else:
-            #print(f'adding {int(stats_static[i][2:]):,} {int(stats_static[i-1][2:]):,} {int(stats_static[i][2:]) - int(stats_static[i - 1][2:]):,}')
+            print(f'adding {int(sim_secs_static[i][2:]):,} {int(sim_secs_static[i-1][2:]):,} {int(sim_secs_static[i][2:]) - int(sim_secs_static[i - 1][2:]):,} {int(sim_insts_static[i]) - int(sim_insts_static[i - 1]):,}')
             total_sim_sec_static += int(sim_secs_static[i][2:]) - int(sim_secs_static[i - 1][2:])
             total_sim_insts_static += int(sim_insts_static[i]) - int(sim_insts_static[i - 1])
+
+    print('\n----\n')
+
+    for i in range(26):
+        if i % 2 == 0:
+            prev_time = int(sim_secs_static_nofp[i][2:])
+            prev_insts = int(sim_insts_static_nofp[i])
+        else:
+            print(f'adding {int(sim_secs_static_nofp[i][2:]):,} {int(sim_secs_static_nofp[i-1][2:]):,} {int(sim_secs_static_nofp[i][2:]) - int(sim_secs_static_nofp[i - 1][2:]):,} {int(sim_insts_static_nofp[i]) - int(sim_insts_static_nofp[i - 1]):,}')
+            total_sim_sec_static_nofp += int(sim_secs_static_nofp[i][2:]) - int(sim_secs_static_nofp[i - 1][2:])
+            total_sim_insts_static_nofp += int(sim_insts_static_nofp[i]) - int(sim_insts_static_nofp[i - 1])
+
+    print('\n----\n')
 
     for i in range(26):
         if i % 2 == 0:
             prev_time = int(sim_secs_dynamic[i][2:])
             prev_insts = int(sim_insts_dynamic[i])
         else:
-            #print(f'adding {int(stats_dynamic[i][2:]):,} {int(stats_dynamic[i - 1][2:]):,} {int(stats_dynamic[i][2:]) - int(stats_dynamic[i - 1][2:]):,}')
+            print(f'adding {int(sim_secs_dynamic[i][2:]):,} {int(sim_secs_dynamic[i-1][2:]):,} {int(sim_secs_dynamic[i][2:]) - int(sim_secs_dynamic[i - 1][2:]):,} {int(sim_insts_dynamic[i]) - int(sim_insts_dynamic[i - 1]):,}')
             total_sim_sec_dynamic += int(sim_secs_dynamic[i][2:]) - int(sim_secs_dynamic[i - 1][2:])
             total_sim_insts_dynamic += int(sim_insts_dynamic[i]) - int(sim_insts_dynamic[i - 1])
 
+    print('\n----\n')
+
     print(f'static: {total_sim_sec_static} - {total_sim_insts_static}')
+    print(f'static_nofp: {total_sim_sec_static_nofp} - {total_sim_insts_static_nofp}')
     print(f'dynamic: {total_sim_sec_dynamic} - {total_sim_insts_dynamic}')
